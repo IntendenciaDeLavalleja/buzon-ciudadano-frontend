2ccf9314ac9065a5b0c63c9923cc06fc4f6585de
IntendenciaDeLavalleja <repositorioIDL@lavalleja.gub.uy>
2026-03-23 13:41:22 -0300

    se puso fijo caminería rurla como opción de selección predeterminada y única de categoría

--- src/features/tickets/components/TicketForm.tsx
+++ src/features/tickets/components/TicketForm.tsx
@@ -3,7 +3,7 @@ import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { motion, AnimatePresence } from "framer-motion";
 import toast from "react-hot-toast";
-import { ticketSchema, type TicketFormData, CATEGORY_MAPPING } from "../schema";
+import { ticketSchema, type TicketFormData } from "../schema";
 import { MapModal } from "./MapModal";
 import { useCreateTicket } from "../hooks";
 
